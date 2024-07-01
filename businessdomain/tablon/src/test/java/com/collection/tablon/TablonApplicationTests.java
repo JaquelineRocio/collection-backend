7e351c210f8de4c8c8470a9eaf517986a99db2c8
JaquelineRocio <jaquelineramosvargas@gmail.com>
2024-07-01 08:41:17 -0500

    creando los endpoints para tablon

---
+++ businessdomain/tablon/src/test/java/com/collection/tablon/TablonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collection.tablon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
